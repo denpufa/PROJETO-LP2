7cc164c100de5744ab8dde109a13dc844ec6f221
Rodolfo Dantas de Oliveira Bezerra <denpufa@A202-10L.imd.local>
2019-10-29 18:39:19 -0300

    tem um erro na classe telegram bot cuidado

--- intelliji/src/projetinho/telegram/com/company/Main.java
+++ intelliji/src/projetinho/telegram/com/company/Main.java
@@ -3,13 +3,22 @@ package projetinho.telegram.com.company;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        TelegramBot tb = new TelegramBot("1001978429:AAHB97aihuye-wDwX09_yW_77jJ6kMPtmfY");
+        try {
+            tb.run();
+        } catch (UnirestException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
-
-
+}
 
 
