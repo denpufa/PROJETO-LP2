b19c53183a4f8db3b51fd8a3dc5f9c8fdaffa1c6
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2019-11-01 20:32:09 -0300

    Create Bot.java

---
+++ intelliji/src/projetinho/telegram/com/company/Bot.java
@@ -0,0 +1,24 @@
+ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+public class Bot extends TelegramLongPollingBot
+
+{
+
+        public void onUpdateReceived(Update update)
+        {
+
+        }
+
+        
+
+        public String getBotUsername()
+        {
+
+           return "WealthManagerBot";
+        }
+
+        public String getBotToken()
+        {
+
+           return "1001978429:AAHB97aihuye-wDwX09_yW_77jJ6kMPtmfY";
+        }
