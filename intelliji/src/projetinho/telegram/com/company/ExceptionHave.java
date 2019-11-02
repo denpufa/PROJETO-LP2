03a949a5253adede49446ebb051800a36b5034bc
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2019-11-02 10:10:40 -0300

    Create ExceptionHave.java

---
+++ intelliji/src/projetinho/telegram/com/company/ExceptionHave.java
@@ -0,0 +1,8 @@
+package projetinho.telegram.com.company;
+public class ExceptionHave extends Exception
+{
+    public String getMessage()
+    {
+        return "esse nome já existe na lista de nomes cadastrados par este tipo"
+     }
+ }
