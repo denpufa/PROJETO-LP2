39dd73dc8be6e15df0a75647455933ad19be6d7e
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2019-11-02 09:33:49 -0300

    Update Menu.java

--- intelliji/src/projetinho/telegram/com/company/Menu.java
+++ intelliji/src/projetinho/telegram/com/company/Menu.java
@@ -25,73 +25,83 @@ public class Menu implements SystemNeeds
         bool permanecer = true;
         while(permanecer)
         {
-        //inicializando objetos da api do telegram.
-        Update u = new Update();
-        SendMessage message = new SendMessage();
-        
-        message.setText("seja bem vindo ao gerenciador escolha uma opção   pelo número,tenha um bom dia "  +
-                        "1-registrar localização" + 
-                        "2-registrar categoria de bem"  +
-                        "3-registrar bem (patrimônio)" +
-                        "4-Listar localizações" +
-                        "5-Listar categorias"  +
-                        "6-Listar bem por categoria" +
-                        "7-Listar bem por localização" +
-                        "8-Procurar patrimonio por código"  +
-                        "9-Procurar patrimonio por nome" +
-                        "10-Procurar patrimonio por descrição" +
-                        "11-Mover patrimonio entre as localizações" +
-                        "12-Gerar relátorio" +
-                        "0-sair do menu");
-        message.setChatId(u.getMessage.getChatId());
-        try
-        {execute(message);}
-        catch(TelegramApiException e)
-        { e.printStackTrace; }
-        
-         String opcao = u.getMessage().getText();
-         //o break no switch é por uma questão de previnir.
-         switch(opcao)
-         {
-         case "0":
-                  permanecer = false;
-                  break;
-         case "1": 
-                  this.registerLocation();
-                  break;
-         case "2":
-                 this.registerCategory();
-                 break;
-         case "3":
-                 this.registerPatrimony();
-                 break;
-         case "4":
-                  this.listLocation();
-                  break;
-         case "5":
-                 this.listCategory();
-                 break;
-         case "6":
-                 this.listPatrimonyByCategory();
-                 break;
-         case "7":
-                 this.listPatrimonyByLocation();
-                 break;
-         case "8":
-                 this.searchPatrimonyByCode();
-                 break;
-         case "9":
-                 this.searchPatrimonyByName();
-                 break;
-         case "10":
-                 this.searchPatrimonyByDescription();
-                 break;
-         case "11":
-                 this.movePatrimony();
-                 break;
-         case "12":
-                 this.generateReport();
-                 break;
+            //inicializando objetos da api do telegram.
+            Update u = new Update();
+            SendMessage message = new SendMessage();
+
+            message.setText("seja bem vindo ao gerenciador escolha uma opção   pelo número,tenha um bom dia "  +
+                            "1-registrar localização" + 
+                            "2-registrar categoria de bem"  +
+                            "3-registrar bem (patrimônio)" +
+                            "4-Listar localizações" +
+                            "5-Listar categorias"  +
+                            "6-Listar bem por categoria" +
+                            "7-Listar bem por localização" +
+                            "8-Procurar patrimonio por código"  +
+                            "9-Procurar patrimonio por nome" +
+                            "10-Procurar patrimonio por descrição" +
+                            "11-Mover patrimonio entre as localizações" +
+                            "12-Gerar relátorio" +
+                            "0-sair do menu");
+            message.setChatId(u.getMessage.getChatId());
+            try
+                {execute(message);}
+            catch(TelegramApiException e)
+                { e.printStackTrace; }
+
+             String opcao = u.getMessage().getText();
+             //o break no switch é por uma questão de previnir.
+             switch(opcao)
+             {
+                 case "0":
+                          permanecer = false;
+                          break;
+                 case "1": 
+                          this.registerLocation();
+                          break;
+                 case "2":
+                         this.registerCategory();
+                         break;
+                 case "3":
+                         this.registerPatrimony();
+                         break;
+                 case "4":
+                          this.listLocation();
+                          break;
+                 case "5":
+                         this.listCategory();
+                         break;
+                 case "6":
+                         this.listPatrimonyByCategory();
+                         break;
+                 case "7":
+                         this.listPatrimonyByLocation();
+                         break;
+                 case "8":
+                         this.searchPatrimonyByCode();
+                         break;
+                 case "9":
+                         this.searchPatrimonyByName();
+                         break;
+                 case "10":
+                         this.searchPatrimonyByDescription();
+                         break;
+                 case "11":
+                         this.movePatrimony();
+                         break;
+                 case "12":
+                         this.generateReport();
+                         break;
+                 default:
+                        message.setText("Voçê não digitou uma opção valída,tente novamente");
+                        try
+                           {execute(message);}
+                        catch(TelegramApiException e)
+                            { e.printStackTrace; }
+                        break;
+               }
+           }
+      }
          
                  
                    
@@ -105,8 +115,10 @@ public class Menu implements SystemNeeds
 
 
     @Override
-    public void registerLocation() {
-
+    public void registerLocation()
+    {
+            
+        
     }
 
     @Override
