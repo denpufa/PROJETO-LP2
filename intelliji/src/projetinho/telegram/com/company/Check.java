635d267742b9004e6cca3bc6b52969805eeaee6c
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2019-11-02 10:20:12 -0300

    Create Check.java

---
+++ intelliji/src/projetinho/telegram/com/company/Check.java
@@ -0,0 +1,15 @@
+
+public class Check throws ExceptionHave
+{
+    public static void checkName(ArrayList a,String s) 
+    {
+        for(int i=0;i<a.size();i++)
+        {
+            if(a.get(i).getName() == s)
+            {
+              throw  new ExceptionHave();
+              break;
+             }
+         }
+       }
+  }
